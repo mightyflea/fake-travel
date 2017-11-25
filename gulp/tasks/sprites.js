8d65ac8679caa621b1046f340f9b00c0b0e43f5e
BJ <bj@f2newmedia.com>
2017-11-25 14:18:22 -0600

    Sprite training complete

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
+var del = require('del');
 
 config = {
 	mode: {
@@ -15,21 +16,35 @@ config = {
 	}
 }
 
-gulp.task('createSprite', function(){
+//delete sprite folder
+gulp.task('beginClean', function(){
+	return del(['./app/temp/sprite','./app/assets/images/sprites']);
+});
+
+//creates the sprite file from .svg files in icons directory, requires 'beginClean' to run first
+gulp.task('createSprite', ['beginClean'], function(){
 	return gulp.src('./app/assets/images/icons/**/*.svg')
 		.pipe(svgSprite(config))
 		.pipe(gulp.dest('./app/temp/sprite/'));
 });
 
+//copies the sprite file from creation dir to app dir, requires 'createSprite' to run first
 gulp.task('copySpriteGraphic', ['createSprite'], function(){
 	return gulp.src('./app/temp/sprite/css/**/*.svg')
 		.pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
+//creates the sprite css file from temp to app, requires 'createSprite' to run first
 gulp.task('copySpriteCSS', ['createSprite'], function(){
 	return gulp.src('./app/temp/sprite/css/*.css')
 		.pipe(rename('_sprite.css'))
 		.pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-gulp.task('icons', ['createSprite', 'copySpriteGraphic', 'copySpriteCSS']);
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
+	return del(['./app/temp/sprite']);
+});
+
+//run all sprite creation procedures
+//note: if a deletion directory is toggled open, even if not selected, deletion will fail
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
