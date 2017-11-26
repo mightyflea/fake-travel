a43636d7e60c24663ac17f7f0f1ae065d50ced40
BJ <bj@f2newmedia.com>
2017-11-26 17:17:42 -0600

    Lessons 37 & 38

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp'),
-	postCSS = require('gulp-postcss'),
-	cssvars = require('postcss-simple-vars'),
-	cssnested = require('postcss-nested'),
-	cssimport = require('postcss-import'),
-	cssmixins = require('postcss-mixins'),
-	autoprefixer = require('autoprefixer');
+	postCSS = require('gulp-postcss'),//runs raw CSS through a post-processor
+	cssvars = require('postcss-simple-vars'),//allows variables in CSS
+	cssnested = require('postcss-nested'),//allows nested specifiers
+	cssimport = require('postcss-import'),//imports included CSS files into a single file
+	cssmixins = require('postcss-mixins'),//allows mixins (kind of like CSS custom tags)
+	autoprefixer = require('autoprefixer'),//replaces "&" with the name of the current class
+	hexrgba = require('postcss-hexrgba');//converts hex values to rgba
+
 
 //run all CSS manipulation tasks
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-					.pipe(postCSS([cssimport, cssmixins, cssvars, cssnested, autoprefixer]))
+					.pipe(postCSS([cssimport, cssmixins, cssvars, cssnested, hexrgba, autoprefixer]))
 					.on('error', function(errorInfo){
 						console.log(errorInfo.toString());
 						this.emit('end');
